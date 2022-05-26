2f414aa9ec517d8298dab90ccec2c820f9c250c5
innfi <innfi.world@gmail.com>
2022-05-26 18:32:20 +0900

    tweaked init_db script

--- webapp-zero2prod/util/init_db.sh
+++ webapp-zero2prod/util/init_db.sh
@@ -1,7 +1,13 @@
+#/bin/bash
+
+set -x
+set -eo pipefail
+
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
+CONTAINER_NAME=innfis-postgres
 
 docker run \
 -e POSTGRES_USER=${DB_USER} \
@@ -9,11 +15,15 @@ docker run \
 -e POSTGRES_DB=${DB_NAME} \
 -p "${DB_PORT}":5432 \
 -d postgres \
+--name ${innfis-postgres} \
 postgres -N 10
 
 export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+until winpty docker exec -it ${innfis-postgres} psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
 >&2 echo "Postgres is still unavailable - sleeping"
 sleep 1
 done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
