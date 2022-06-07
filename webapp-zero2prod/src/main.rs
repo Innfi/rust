32443d0ed544aaad5ce83a42eea8f349ae70795d
innfi <innfi.world@gmail.com>
2022-06-08 07:48:26 +0900

    higher rank trait bound? (4.5.11)

--- webapp-zero2prod/src/main.rs
+++ webapp-zero2prod/src/main.rs
@@ -7,20 +7,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-  // LogTracer::init().expect("failed to set logger");
-
-  // let env_filter = EnvFilter::try_from_default_env()
-  //   .unwrap_or_else(|_| EnvFilter::new("info"));
-  // let formatting_layer = BunyanFormattingLayer::new(
-  //   "zero2prod".into(),
-  //   std::io::stdout
-  // );
-  // let subscriber = Registry::default()
-  //   .with(env_filter)
-  //   .with(JsonStorageLayer)
-  //   .with(formatting_layer);
-  // set_global_default(subscriber).expect("failed to set subscriber");
-  let subscriber = get_subscriber("zero2prod".into(), "info".into());
+  let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
   init_subscriber(subscriber);
 
   let configuration = get_configuration().expect("failed to read configurations");
--- webapp-zero2prod/tests/health_check.rs
+++ webapp-zero2prod/tests/health_check.rs
@@ -1,17 +1,25 @@
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
+use once_cell::sync::Lazy;
 
 use zero2prod;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 pub struct TestApp {
   pub address: String,
   pub db_pool: PgPool,
 }
 
+static TRACING: Lazy<()> = Lazy::new(|| {
+  let subscriber = get_subscriber("test".into(), "debug".into());
+  init_subscriber(subscriber);
+});
+
 async fn spawn_app() -> TestApp {
+  Lazy::force(&TRACING);
   let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
   let port = listener.local_addr().unwrap().port();
   let address = format!("http://127.0.0.1:{}", port);
