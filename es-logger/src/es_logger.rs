7fb70a480b5360d57f6c9e77a6ad99181fa1f00b
innfi <innfi.world@gmail.com>
2022-09-22 22:07:13 +0900

    start writing a custom logger using es

---
+++ es-logger/src/es_logger.rs
@@ -0,0 +1,34 @@
+use elasticsearch::{
+  Error,
+	Elasticsearch, 
+	http::transport::Transport, 
+	IndexParts,
+};
+use serde_json::json;
+
+pub struct Logger {
+  client: Elasticsearch,
+}
+
+impl Logger {
+  pub fn new(url: &str) -> Self {
+    Self {
+      client: Elasticsearch::new(Transport::single_node(url).unwrap())
+    }
+  }
+
+  // TODO: log level, debug / trace / error
+  // reconnect logic?
+
+  async fn info(&self, payload: &str) -> Result<(), Error> {
+    self.client.index(IndexParts::IndexId("temp", "1"))
+      .body(json!({
+        "id": 1,
+        "payload": payload,
+      }))
+      .send()
+      .await?;
+
+    Ok(())
+  }
+}
---
+++ es-logger/src/main.rs
@@ -0,0 +1,5 @@
+use es_logger::es_logger;
+
+fn main() {
+    println!("Hello, world!");
+}
