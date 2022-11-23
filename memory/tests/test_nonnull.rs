2eed034128dc3aa1b6b0284f966abaea892c59ed
innfi <innfi.world@gmail.com>
2022-11-23 20:39:49 +0900

    memory: it seems that unsafe is unavoidable

--- memory/tests/test_nonnull.rs
+++ memory/tests/test_nonnull.rs
@@ -21,8 +21,8 @@ struct Node<T> {
 }
 
 #[test]
-fn test_as_mut_for_struct() {
-  let mut first = Some(
+fn test_as_ptr_for_struct() {
+  let first = Some(
     NonNull::new(&mut Node {
       elem: 1,
       prev: None,
@@ -30,7 +30,7 @@ fn test_as_mut_for_struct() {
     }).expect("empty")
   );
 
-  let mut second = Some(
+  let second = Some(
     NonNull::new(&mut Node {
       elem: 2,
       prev: None,
@@ -43,4 +43,31 @@ fn test_as_mut_for_struct() {
 
   let second_ref = second.unwrap().as_ptr();
   unsafe { (*second_ref).prev = first };
+
+  let end_of_chain = unsafe { first.unwrap().as_ref().next.unwrap().as_ref() };
+  assert_eq!(end_of_chain.elem, 2);
+}
+
+#[test]
+fn test_as_mut_for_struct() {
+  let first = Some(
+    NonNull::new(&mut Node {
+      elem: 1,
+      prev: None,
+      next: None,
+    }).expect("empty")
+  );
+
+  let second = Some(
+    NonNull::new(&mut Node {
+      elem: 2,
+      prev: None,
+      next: None,
+    }).expect("empty")
+  );
+
+  unsafe { first.unwrap().as_mut().next = second };
+
+  let end_of_chain = unsafe { first.unwrap().as_ref().next.unwrap().as_ref() };
+  assert_eq!(end_of_chain.elem, 2);
 }
